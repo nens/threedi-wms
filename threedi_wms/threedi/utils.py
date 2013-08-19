5e5c7b412416ff1bca2127b329818c19d7436927
Jack Ha <jackha@gmail.com>
2013-08-19 16:21:47 +0200

    Reads projection from geotiff, defaults to RD

--- threedi_wms/threedi/utils.py
+++ threedi_wms/threedi/utils.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import
 from __future__ import division
 
 import os
+import gdal
+import osr
 
 from threedi_wms.threedi import config
 
@@ -57,3 +59,13 @@ def get_pyramid_path(layer):
 def get_monolith_path(layer):
     """ Return monolith path. """
     return os.path.join(config.CACHE_DIR, layer, 'monolith')
+
+
+def get_bathymetry_srs(filename):
+    """Return srs from bathymetry, None if not set"""
+    ds = gdal.Open(filename, gdal.GA_ReadOnly)
+    src = osr.SpatialReference()
+    src.ImportFromWkt(ds.GetProjection())
+    result = src.GetAttrValue(str('PROJCS|AUTHORITY'), 1)  # None or '22234'
+    ds = None  # Close dataset
+    return result
